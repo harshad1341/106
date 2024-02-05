259b33fbcbf069f4db401628829c698097e52f99
harshad1341 <85162262+harshad1341@users.noreply.github.com>
2024-02-05 23:10:30 +0530

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,32 @@
+import cv2
+
+# Create our body classifier
+body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+# Initiate video capture for video file
+cap = cv2.VideoCapture('walking.avi')
+
+# Loop once the video is successfully loaded
+while True:
+    
+    # Read the first frame
+    ret, frame = cap.read()
+
+    # Convert Each Frame into Grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Pass frame to our body classifier
+    bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
+    
+    # Extract bounding boxes for any bodies identified
+    for (x, y, w, h) in bodies:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)  # Draw rectangle around the detected body
+    
+    # Display the frame with rectangles drawn around detected bodies
+    cv2.imshow('Pedestrian Detection', frame)
+
+    if cv2.waitKey(1) == 32:  # 32 is the Space Key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
